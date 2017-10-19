831366fff09022963dccee44910e6fc276686f5a
xiejiyao <470267864@qq.com>
2017-10-19 15:26:06 +0800

    add note(注释)

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const casper = require('casper').create({
     clientScript: ['jquery-1.8.3.min.js']
-});
-phantom.outputEncoding = "gbk";
-    var page = casper.cli.get(0);
-        var shopInfoObj;
-        var url = 'http://pub.alimama.com/promo/item/channel/index.htm?spm=a219t.7900221%2F1.1998910419.d07bd19c4.15e6cd4afzMh0G&channel=nzjh&toPage=' + page + '&perPageSize=40';
+});//创建一个casper实例，并且使用jquery来在和网页连接时使用
+phantom.outputEncoding = "gbk";//防止乱码？
+        var page = casper.cli.get(0);//casper.cli.get(x)取到命令行第x位的参数，casper.cli.has(x)检测命令行是否有第x位参数或者是否有参数为x
+        var shopInfoObj;//这个全局变量是为了之后存储爬到的数据
+        var url = 'http://pub.alimama.com/promo/item/channel/index.htm?spm=a219t.7900221%2F1.1998910419.d07bd19c4.15e6cd4afzMh0G&channel=nzjh&toPage=' + page + '&perPageSize=40';//url
         casper.start(url);//打开浏览器
 
         // casper.waitForSelector('.search-box-img.img-loaded', function() {
@@ -13,31 +13,29 @@ phantom.outputEncoding = "gbk";
         //     this.echo('capture is done');
         // })
 
-        casper.wait(15000,function(){
+        casper.wait(15000,function(){//等待15秒。。为了网页加载玩，waitForSelector不知为何用了网页就不能加载了- -
             this.echo('the web is runing,please wait...');
-            this.capture(page + '.png');//查看网页状况，一搬5s差不多了
+            this.capture(page + '.png');//等待完成了截个图看看网页加载状况
             this.echo('capture is done');
         })
 
-        casper.then(function(){
-            shopInfoObj = casper.evaluate(getInfo);
+        casper.then(function(){//然后
+            shopInfoObj = casper.evaluate(getInfo);//按我的理解（手动滑稽），casper.evaluate()这个方法就好比你进到网页控制台然后执行你自己的方法来进行网页操作
         })
 
-        casper.then(function(){
-            console.log(JSON.stringify(shopInfoObj));
-            if(shopInfoObj.length>0){
-                casper.thenOpen('http://localhost:3000/dataInit',{
+        casper.then(function(){//然后
+            if(shopInfoObj.length>0){//我们这时候应该差不多爬取到数据了。。长度大于0的话就打开我们的提交接口
+                casper.thenOpen('http://localhost:3000/dataInit',{//然后使用post来把我们爬到的数据提交
                     method:'post',
                     data:{
-                        info:JSON.stringify(shopInfoObj)
+                        info:JSON.stringify(shopInfoObj)//提交做字符串处理
                     }
                 })
             }
         })
-        casper.then(function(){
+        casper.then(function(){//然后
              this.echo('post it');
-             this.echo(shopInfoObj);
-             casper.exit();
+             casper.exit();//提交完成后我们关掉浏览器。。
          })
         
 
@@ -48,7 +46,7 @@ phantom.outputEncoding = "gbk";
         // })
 
 
-        function getInfo(){
+        function getInfo(){//dom操作
             var arr = [];
             $.each($('.block-search-box'),function(i,items){
                 var obj = {};
@@ -65,5 +63,5 @@ phantom.outputEncoding = "gbk";
             return arr;
         }
 
-        casper.run();
+        casper.run();//要有run才可以执行哦
     
--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const cookieSession = require('cookie-session');
 const child_process = require("child_process");
 
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false })//使用bodyParser来进行post参数的处理
+
 var arr = [];
 
 for(var i=0;i<10000;i++){
@@ -18,21 +19,21 @@ server.listen(3000,function(){
     console.log('server is run in 3000');
 })
 
-server.use(express.static('./views'));
+server.use(express.static('./views'));//使用express-static来处理静态文件，中间件好像已经被集成？？
 server.use(urlencodedParser);
-server.use(cookieParser());
-server.use(cookieSession({keys:arr}))
+server.use(cookieParser());//cookie-parser和cookie-session来进行cookie和sission的操作
+server.use(cookieSession({keys:arr}))//这里本来是想用session来记录写入次数然后想到一个全局变量就搞定就暂时废弃，等修炼到金丹再来- -
 
-var page = 1
+var page = 1//由于女装模块的url是文件最下面的那种规律所以，从第一页开始首先定义一个全局page=1
 
-server.use('/start',function(req,res){
+server.use('/start',function(req,res){//接受请求来开始爬虫
     console.log('the pc is runing');
-    if(page <= 100) {
-        child_process.exec(`casperjs main.js ${page}`);
-        res.send(JSON.stringify('{"r":"200","msg":"操作成功"}'))
+    if(page <= 100) {//由于我看到只有100页数据，然后就没有下一页了，so，定了100页
+        child_process.exec(`casperjs main.js ${page}`);//child_process.exec(): 衍生一个 shell 并在 shell 上运行命令，当完成时会传入 stdout 和 stderr 到回调函数。  前面的是node官网的解释，按我的理解（手动滑稽）这里也就是生成一个子进程来在命令行执行 ｛casperjs main.js 参数｝这一条命令
+        res.send(JSON.stringify('{"r":"200","msg":"操作成功"}'));//给前端返回。。
         res.end();
 
-    }else {
+    }else {//当爬取完毕后。。。
         console.log('the end');
         res.send(JSON.stringify('{"r":"500","msg":"page too large"}'))
         res.end();
@@ -40,9 +41,9 @@ server.use('/start',function(req,res){
 })
 
 
-server.use('/dataInit',function(req,res){
-    console.log(req.body);
-    var strobj = JSON.stringify(req.body);
+server.use('/dataInit',function(req,res){//这个接口主要进行文件写入操作，main.js爬取的数据最终会通过表单post提交的方式提交到这个接口。每爬取一页就会写入一次
+    console.log(req.body);//在这里我们就可以看见爬去来的数据
+    var strobj = JSON.stringify(req.body);//fs写入文件需要字符串，所以这里做了处理
     // if(req.session['howmuch']==null){
     //     fs.writeFile('./jsonFile/'+page+'.json',strobj,function(err){
     //         if(err){
@@ -65,14 +66,14 @@ server.use('/dataInit',function(req,res){
     //     })
     // }
 
-    fs.writeFile('./jsonFile/'+page+'.json',strobj,function(err){
+    fs.writeFile('./jsonFile/'+page+'.json',strobj,function(err){//写入文件，当爬取到page就把数据写入文件page.json
         if(err){
             console.log(err);
             return false;
         }else{
             console.log('写入成功');
-            page ++;
-            res.redirect('/start');
+            page ++;//当文件写入成功后就可以继续爬取下一页，所以page++
+            res.redirect('/start');//这个是调用自己的静态路由接口，再次去新建一个子进程创建一个casper实例去继续爬取下一页数据
         }
     })
 })
